e7236459c235f6c3709c4cc898dfa5eb9bbf0531
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-01-02 16:49:02 +0100

    Server action practice

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,22 @@
 const OrganizationIdPage = () => {
-  return <div>Organization page</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    console.log("I'm triggered");
+  }
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          required
+          placeholder="Enter a board title"
+          className="border-black border pt-1"
+        />
+      </form>
+    </div>
+  );
 };
 export default OrganizationIdPage;
