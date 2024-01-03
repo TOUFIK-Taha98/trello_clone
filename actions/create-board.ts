9132c9286f8d148ef989c9eabf963dfc446f2301
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-01-03 11:21:12 +0100

    Erros implementation with Zod in the server action

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -2,22 +2,48 @@
 
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
+export type State = {
+  errors?: {
+    title?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateBoard = z.object({
-  title: z.string(),
+  title: z.string().min(3, {
+    message: "Minimum length of 3 letters is required",
+  }),
 });
 
-export async function create(formData: FormData) {
-  const { title } = CreateBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = CreateBoard.safeParse({
     title: formData.get("title"),
   });
 
-  await db.board.create({
-    data: {
-      title,
-    },
-  });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing required fields",
+    };
+  }
+
+  const { title } = validatedFields.data;
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Database error",
+    };
+  }
 
   revalidatePath("/organization/org_2aOtzuLtUqAtXgHJJissmgpMLG0");
+  redirect("/organization/org_2aOtzuLtUqAtXgHJJissmgpMLG0");
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -2,24 +2,14 @@ import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 import Board from "./Board";
+import { Form } from "./form";
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
 
-  console.log(boards);
   return (
     <div className="flex flex-col space-y-4">
-      <form action={create}>
-        <input
-          id="title"
-          name="title"
-          type="text"
-          required
-          placeholder="Enter a board title"
-          className="border-black border pt-1"
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form />
 
       <div className="space-y-2">
         {boards.map((board) => (
