9b098b08fe676cfddb96f7d68b49ad694319fb70
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-01-04 10:19:15 +0100

    Create a new List

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { ListWithCards } from "@/types";
-import { List } from "@prisma/client";
+import ListForm from "./list-form";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
 }
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
-  return <div>ListContainer</div>;
+  return (
+    <ol>
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  );
 };
 export default ListContainer;
