43f55775a5bafcb2f051b315f3dd32de5bb0efeb
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-01-04 10:28:12 +0100

    Fetch list title after creation

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { ListWithCards } from "@/types";
 import ListForm from "./list-form";
+import ListItem from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
 }
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-4">
+      {orderedData.map((list, index) => {
+        return <ListItem index={index} key={list.id} data={list} />;
+      })}
       <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol>
