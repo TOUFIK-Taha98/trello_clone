e0e5f3c7d084a57f612dac722c5ec2066d81a702
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-01-04 12:14:48 +0100

    DESGIN - Drag'n drop Lists & Cards

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 import { ListWithCards } from "@/types";
 import ListForm from "./list-form";
 import ListItem from "./list-item";
@@ -18,13 +18,24 @@ const ListContainer = ({ data, boardId }: ListContainerProps) => {
   }, [data]);
 
   return (
-    <ol className="flex gap-x-3 h-4">
-      {orderedData.map((list, index) => {
-        return <ListItem index={index} key={list.id} data={list} />;
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-4"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem index={index} key={list.id} data={list} />;
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 export default ListContainer;
